b4950306d949e84900a9759c9d579f49a32c7b45
Robin Bette <robinbette@hotmail.com>
2022-01-20 13:39:01 +0100

    format

--- WiredBrain/Hubs/CoffeeHub.cs
+++ WiredBrain/Hubs/CoffeeHub.cs
@@ -1,13 +1,11 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.SignalR;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.SignalR;
 using WiredBrain.Helpers;
 
 namespace WiredBrain.Hubs
 {
-    public class CoffeeHub: Hub
+    public class CoffeeHub : Hub
     {
         private readonly OrderChecker _orderChecker;
 
@@ -19,14 +17,19 @@ namespace WiredBrain.Hubs
         public async Task GetUpdateForOrder(int orderId)
         {
             CheckResult result;
+
             do
             {
                 result = _orderChecker.GetUpdate(orderId);
+
                 Thread.Sleep(1000);
+
                 if (result.New)
                     await Clients.Caller.SendAsync("ReceiveOrderUpdate", 
                         result.Update);
-            } while (!result.Finished);
+            } 
+            while (!result.Finished);
+
             await Clients.Caller.SendAsync("Finished");
         }
     }
